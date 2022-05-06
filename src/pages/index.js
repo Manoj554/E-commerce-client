4fe4277beb0e984611b0e22aa011a0d209255b72
Manoj <manojkumarmondal554@gmail.com>
2022-05-06 21:55:48 +0530

    landing page included

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from '../contents/Landing/Landing';
+
+const index = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
+
+export default index
