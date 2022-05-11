6153507bf80ece36ae75916f658323fc9da5aaa1
Manoj <manojkumarmondal554@gmail.com>
2022-05-11 12:42:01 +0530

    no/children error fixed in Recombar

--- src/components/Header/Recombar.jsx
+++ src/components/Header/Recombar.jsx
@@ -5,17 +5,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts, getProductByCategory, getProductsByRootCategory } from '../../redux/actions';
 import { useRouter } from 'next/router';
 
-const SubCategory = ({ id, name, children, handleClick, i, handleClick2 }) => {
+const SubCategory = ({ id, name, child, handleClick, i, handleClick2 }) => {
 	return (
 		<>
 			<div key={id} className={styles.content} id={i % 2 == 0 ? styles.violet : styles.orange}>
 				<span onClick={handleClick2.bind(this, { name: name, id: id })}>
 					{name}
 				</span>
-				{children.length > 0 && (
+				{child.length > 0 && (
 					<>
 						<div className={styles.suggestion}>
-							{children.map((val) => (
+							{child.map((val) => (
 								<div key={val.id} className={styles.suglst} onClick={handleClick.bind(this, { name: val.name, id: val.id })} >{val.name}
 								</div>
 							))}
@@ -53,7 +53,7 @@ const Recombar = () => {
 				</div>
 				{loading ? <p>Loading categories ...</p> :
 					categories.length > 0 && categories.map((val, i) => (
-						<SubCategory key={val.id} id={val.id} name={val.name} children={val.children} handleClick={handleClick} handleClick2={handleClick2} i={i} />
+						<SubCategory key={val.id} id={val.id} name={val.name} child={val.children} handleClick={handleClick} handleClick2={handleClick2} i={i} />
 					))}
 			</div>
 		</>
