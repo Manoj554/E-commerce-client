656cc11a73d635e096f75de562d3ecd9b3513305
Manoj <manojkumarmondal554@gmail.com>
2022-05-19 21:52:22 +0530

    meta tag included for SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,13 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>CartAny</title>
                 <link rel="shortcut icon" href="/Cartanyorangecrop.png" type="image/x-icon" />
+                <meta name="description" content="CartAny | Bringing the art into the Cart | A E-Commerce site for online shoping" />
+                <meta name="keywords" content="online-shoping cartany ecommerce cart-your-product" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+                <meta name="google-site-verification" content="wOxpzwAmfE_PQcXsJoKNSrzG6ALvBzXFwlqbOhbIDKw" />
             </Head>
+
             <Provider store={store}>
                 <Layout>
                     <Component {...pageProps} />
